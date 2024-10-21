5b0d52f0836db610f14cf34e326d021f92aacdac
SashaMixaylov <alexandr_mixalov@mail.ru>
2024-10-21 20:28:47 +0300

    Выводит список файлов

--- main.py
+++ main.py
@@ -8,5 +8,20 @@
 
 import os
 import sys
+import zipfile
+from pathlib import Path
 
-print(os.path.dirname(os.path.abspath(sys.argv[0])))
+print(os.path.dirname(os.path.abspath(sys.argv[0])))  # путь где лежит файл main.py
+
+zip = "croco-blitz-source.zip"
+def see():
+    folder = os.path.dirname(os.path.realpath(__file__))
+    try:
+        with zipfile.ZipFile(Path(folder) / 'src' / zip) as archive:
+            for n in archive.namelist():
+                print(n)
+    except zipfile.BadZipFile as archive:
+        print("error")
+
+if __name__ == '__main__':
+    see()
