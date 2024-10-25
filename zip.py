0d2bf455b26d7785685e106a74d026b91e5e1127
MatveyBelaev2115 <golubevdanil2009@gmail.com>
2024-10-25 19:27:57 +0300

    Update zip.py
    Задача 7 дополнение

--- zip.py
+++ zip.py
@@ -4,6 +4,10 @@
 #
 # Откройте файл презентации в программе с помощью объекта Presentation библиотеки pptx
 
+# Задача 2024.10.23.07
+#
+# Дополните этот код, положите презентацию рядом с main.py и попробуйте запустить программу и открыть объект Presentation, в нём shape, а в нём slide и в нём найти где лежит текст. С помощью дебаггера.
+
 import zipfile
 from pptx import Presentation
 from pathlib import Path
@@ -26,21 +30,28 @@ def extract():
             extracted_file = archive.extract(presentation_files[0])
 
             # Создаем экземпляр Presentation
-            prs = Presentation(str(extracted_file))
+            prs = Presentation('Osennyaya_igra_3.pptx')
 
             # Обрабатываем содержимое презентации
+            # for slide in prs.slides:
+            #     for shape in slide.shapes:
+            #         if hasattr(shape, 'text'):
+            #             print(shape.text)
+            #
+            # return prs
             for slide in prs.slides:
                 for shape in slide.shapes:
-                    if hasattr(shape, 'text'):
-                        print(shape.text)
+                    if not shape.has_text_frame:  
+                        continue
+                    print(shape.text_frame)
 
-            return prs
 
     except zipfile.BadZipFile as archive:
         print("error")
 
 if __name__ == '__main__':
     extract()
+
 
 
 
