3ff21064769fc7dee669ba08d6b158f1ec9b2dc9
MatveyBelaev2115 <golubevdanil2009@gmail.com>
2024-11-01 21:12:42 +0300

    удалены файлы
    .

--- zips.py
+++ zips.py
@@ -5,8 +5,17 @@
 # То есть ваша программа должна работать, когда вы поулчаете на вход zip архив, достаёте из него все pptx файлы и парсите каждый,
 # сохраняете из него все слова, и после парсинга всех презентаций сохраняете все слова в words.txt
 
-
+# Задача 2024.11.01.02
+#
+# В какой контейнер данных нужно собирать слова чтобы исключить дубликаты?
+# Поменяйте код так, чтобы это сделать.
+#
+# Подумайте на каком уровне лучше это делать и почему.
+#
+# Напишите сколько было слов всего из всех файлов, когда вы сохраняли их в список, и сколько стало, когда вы исключили дубликаты.
 import zipfile
+from enum import unique
+
 from pptx import Presentation
 from pathlib import Path
 import os
@@ -23,7 +32,7 @@ def extract(zip_path):
                 return None
 
 
-
+            unique_words = set()
             words = []
             for i, presentation_files in enumerate(presentation_files):
                 prs = Presentation(archive.open(presentation_files))
@@ -31,19 +40,29 @@ def extract(zip_path):
                     for shape in slide.shapes:
                         if hasattr(shape, 'text'):
                          text = shape.text
-                         if " " in text or ":" in text or "БЛИЦ-КРОКОДИЛ" in text:
+                         if " " in text or ":" in text or "БЛИЦ-КРОКОДИЛ" in text or "СУПЕРКРОКО" in text:
                              continue
-                         words.append(text)
-                         print(text)
+                         word = text.strip()
+                         if word and word not in unique_words:
+                             unique_words.add(word)
+                         words.append(word)
+                         print(word)
 
-            return words
+            return list(words)
+            #ищем дубликаты
+            # unique_words = []
+            # for w in words:
+            #     if word not in unique_words:
+            #         unique_words.append(word)
+            #         print(f"Дубликаты {w}")
+
 
     except zipfile.BadZipFile:
         print("Ошибка при открытии ZIP-файла")
         return None
 
 
-# Основная часть программы
+
 if __name__ == '__main__':
     zip_name = "croco-blitz-source.zip"
     result = extract(zip_name)
